4eb7116e60d0f5438eb8113f7afcb0b9aecf1952
Marcel Schramm <marceloschr@googlemail.com>
2021-12-17 13:23:07 +0100

    Posts are now cached

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/headzoo/surf v1.0.1
+	github.com/kennygrant/sanitize v1.2.4
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
 )
 
--- backend/main.go
+++ backend/main.go
@@ -3,42 +3,95 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	chi "github.com/go-chi/chi/v5"
 	"github.com/headzoo/surf"
+	"github.com/kennygrant/sanitize"
 	"golang.org/x/net/html"
 )
 
+var cacheDir string
+
 func main() {
+	baseCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		log.Println("Couldn't find cache directory.")
+		panic(err)
+	}
+	cacheDir = filepath.Join(baseCacheDir, "unmedium")
+	cacheDirError := os.MkdirAll(cacheDir, 0777)
+	if cacheDirError != nil {
+		log.Printf("Error creating cache direction %s:\n\t%s\n", cacheDir, cacheDirError)
+	}
+	log.Printf("Caching into %s\n", cacheDir)
+
 	r := chi.NewRouter()
 	r.Get("/*", post)
 	http.ListenAndServe(":8080", r)
 }
 
+func getPost(postURL string) (*goquery.Selection, *goquery.Selection, error) {
+	baseName := sanitize.BaseName(postURL)
+	postFileName := filepath.Join(cacheDir, baseName)
+	file, openError := os.Open(postFileName)
+	if openError != nil {
+		bow := surf.NewBrowser()
+		mediumOpenError := bow.Open(postURL)
+		if mediumOpenError != nil {
+			return nil, nil, mediumOpenError
+		}
+
+		dom := bow.Dom()
+		//Waiting for article to load, since it's dynamically loaded via JS.
+		//This has to happen before caching, since we might cache it without the article otherwise.
+		var article *goquery.Selection
+		for {
+			if article != nil {
+				break
+			}
+
+			time.Sleep(100 * time.Millisecond)
+			article = dom.Find("article")
+		}
+
+		if os.IsNotExist(openError) {
+			log.Printf("Caching %s\n", postURL)
+			cacheWriteError := os.WriteFile(postFileName, []byte(toHtml(dom)), 0666)
+			if cacheWriteError != nil {
+				log.Printf("Error caching %s:\n\t%s\n", postURL, cacheWriteError)
+			}
+		} else {
+			log.Printf("Error caching %s:\n\t%s\n", postURL, openError)
+		}
+
+		return dom, article, nil
+	}
+
+	defer file.Close()
+
+	log.Printf("Cache hit for %s\n", postURL)
+	document, readError := goquery.NewDocumentFromReader(file)
+	if readError != nil {
+		return nil, nil, readError
+	}
+	return document.Selection, document.Find("article"), nil
+}
+
 func post(w http.ResponseWriter, r *http.Request) {
 	mediumPostURL := chi.URLParam(r, "*")
-	bow := surf.NewBrowser()
-	mediumOpenError := bow.Open(mediumPostURL)
-	if mediumOpenError != nil {
-		http.Error(w, fmt.Sprintf("error request medium post: %s", mediumOpenError.Error()), http.StatusInternalServerError)
+	document, article, postRetrievalError := getPost(mediumPostURL)
+	if postRetrievalError != nil {
+		http.Error(w, fmt.Sprintf("error retrieving medium post: %s", postRetrievalError.Error()), http.StatusInternalServerError)
 		return
 	}
 
-	authorTag := bow.Find("meta[name=author]").First()
-
-	//Waiting for article to load, since it's dynamically loaded via JS.
-	var article *goquery.Selection
-	for {
-		if article != nil {
-			break
-		}
-
-		time.Sleep(1 * time.Second)
-		article = bow.Find("article")
-	}
+	authorTag := document.Find("meta[name=author]").First()
 
 	//Remove author and post meta data from view
 	//FIXME Find safer way to do this.
@@ -52,8 +105,6 @@ func post(w http.ResponseWriter, r *http.Request) {
 	//will flatten the post, it won't cause any issues for now.
 	article = article.Find("section").Children()
 
-	w.WriteHeader(http.StatusOK)
-
 	var html bytes.Buffer
 	html.WriteString("<html><head>")
 	if authorTag.Length() == 1 {
@@ -64,6 +115,7 @@ func post(w http.ResponseWriter, r *http.Request) {
 	html.WriteString(toHtml(article))
 	html.WriteString("</body></html>")
 
+	w.WriteHeader(http.StatusOK)
 	w.Write(html.Bytes())
 }
 
